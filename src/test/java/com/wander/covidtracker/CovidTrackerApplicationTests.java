00177eb537074912e6096f85e27d969ff954a863
rounak <rounakraj1@gmail.com>
2021-01-30 22:18:46 +0530

    Initial commit

---
+++ src/test/java/com/wander/covidtracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander.covidtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
