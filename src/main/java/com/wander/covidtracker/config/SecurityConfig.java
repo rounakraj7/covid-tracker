2aad85917658a64e1eb4d28b33d31586f0e2dcd1
rounak <rounakraj1@gmail.com>
2021-02-09 01:25:48 +0530

    Add configuration for jar creation by combining react

--- src/main/java/com/wander/covidtracker/config/SecurityConfig.java
+++ src/main/java/com/wander/covidtracker/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                         .permitAll()
-                    .antMatchers("/api/auth/**")
+                    .antMatchers("/api/auth/**", "/login", "/home")
                         .permitAll()
                     .anyRequest()
                         .authenticated();
--- src/main/java/com/wander/covidtracker/config/WebMvcConfig.java
+++ src/main/java/com/wander/covidtracker/config/WebMvcConfig.java
@@ -1,7 +1,13 @@
 package com.wander.covidtracker.config;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +21,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+                    "/"));
+        };
     }
 }
